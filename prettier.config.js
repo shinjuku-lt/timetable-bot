62861915f649287244e696e6e9e99a1315dfaa5e
Toshiki Takezawa <tsk.take815@gmail.com>
2018-02-16 23:59:59 +0900

    Add formatter by prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 128,
+    tabWidth: 4,
+    singleQuote: true,
+    semi: false,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+}
